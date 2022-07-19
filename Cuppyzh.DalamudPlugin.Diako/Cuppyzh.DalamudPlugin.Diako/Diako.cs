08b7887d401942bec31535fb0eaa1d778613c862
Cuppyzh <yapaditama@gmail.com>
2022-07-19 12:58:32 +0700

    Re-Init
    - Change structure and dependencies

---
+++ Cuppyzh.DalamudPlugin.Diako/Cuppyzh.DalamudPlugin.Diako/Diako.cs
@@ -0,0 +1,14 @@
+﻿using Dalamud.Plugin;
+
+namespace Cuppyzh.DalamudPlugin.Diako
+{
+    public class Diako : IDalamudPlugin
+    {
+        public string Name => "Diako";
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
